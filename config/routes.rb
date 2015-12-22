4ad720b188e31e6ad35d90a2787fccc3edf6597c
Ilya Kardapolov <kardapolov@gmail.com>
2015-12-22 17:05:30 +0500

    Second commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
+  # You can have the root of your site routed with "root" t
   root 'index#index'
 
   # Example of regular route:
